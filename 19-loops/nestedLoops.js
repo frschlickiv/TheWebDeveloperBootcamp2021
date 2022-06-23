761e0387022807473a3e60f9d0292fa9cce7c686
frschlickiv <frschlickiv@gmail.com>
2022-06-22 23:08:32 -0400

    A secret message for Micks

--- 19-loops/nestedLoops.js
+++ 19-loops/nestedLoops.js
@@ -1,3 +1,5 @@
+//We have a deeply nested comment to let you all know that MICKEY is the one who did it to Blake's no no zone.
+
 // for (let i = 1; i<=10; i++){
 //     console.log(`i is: ${i}`)
 //     for(let j = 1; j < 4; j++) {
@@ -5,17 +7,16 @@
 //     }
 // }
 
-
 const seatingChat = [
-    ['Kristen', 'Erik', 'Namita',], 
-    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
-    ['Yuma', 'Sakura', 'Jack', 'Erika']
-]
+  ["Kristen", "Erik", "Namita"],
+  ["Geoffrey", "Juanita", "Antonio", "Kevin"],
+  ["Yuma", "Sakura", "Jack", "Erika"],
+];
 
-for (let i = 0; i < seatingChart.length[i]; i++){
-    const row = seatingChart.length[i]
-    console.log(`ROW ${i+1}`)
-    for (let j = 0; j < row.length; j++){
-        console.log(row[j])
-    }
+for (let i = 0; i < seatingChart.length[i]; i++) {
+  const row = seatingChart.length[i];
+  console.log(`ROW ${i + 1}`);
+  for (let j = 0; j < row.length; j++) {
+    console.log(row[j]);
+  }
 }
